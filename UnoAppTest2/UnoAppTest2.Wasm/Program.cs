8dd2376a90fa1070231eb5c3ed8d874af09aa043
jquanj <jacobo.quan@jci.com>
2022-11-29 09:24:42 -0500

    Initial commit.

---
+++ UnoAppTest2/UnoAppTest2.Wasm/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+
+namespace UnoAppTest2.Wasm
+{
+    public class Program
+    {
+        private static App _app;
+
+        static int Main(string[] args)
+        {
+            // This statement triggers the issue
+            //SQLitePCL.Batteries.Init();
+
+            // This statement also triggers the same issue
+            SQLitePCL.Batteries_V2.Init();
+
+            // Triggers a System.Exception: Failed to run emscripten
+            //SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
+
+            Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+
+            return 0;
+        }
+    }
+}
